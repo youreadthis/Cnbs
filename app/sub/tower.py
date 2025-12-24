cc284b8e4563a124689f8d488296775ab4f074bd
Verxaoc <nsvladislavmobile@gamil.com>
2025-12-25 00:03:43 +0300

    поменял считывание csv на xlsx и добавил 2 библиотеки

--- app/sub/tower.py
+++ app/sub/tower.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import sys
+import pandas as pd
 
 
 class Tower():
@@ -12,8 +13,19 @@ class Tower():
         self.type_antenna = type_antenna
         self.range_of_handrover = range_of_handrover
         self.standart = standart
-        
 
+
+def parsing_base_station(filename:str="Базовыестанции.xlsx"):
+    script_dir = os.path.dirname(sys.argv[0])
+    df = pd.read_excel(os.path.join(script_dir, filename), index_col=0)
+    towers = []
+    for row in df.itertuples(index=True, name='Row'):
+        towers.append(Tower(row[0], row[1], str(row[2]).replace(",", "."), row[3], row[4], row[5], row[6]))
+    return sorted(towers, key=lambda x: x.square, reverse=True) 
+
+print(parsing_base_station()[2])
+
+"""
 def parsing_base_station(filename:str="Базовыестанции.csv"):
     script_dir = os.path.dirname(sys.argv[0])
 
@@ -24,3 +36,4 @@ def parsing_base_station(filename:str="Базовыестанции.csv"):
         for row in reader:
             towers.append(Tower(row[0], row[1], row[2].replace(",", "."), row[3], row[4], row[5], row[6]))  #заносим всё в массив для работы
         return sorted(towers, key=lambda x: x.square, reverse=True)                                                       #возращаем отсортированный по площаде покрытия
+"""
